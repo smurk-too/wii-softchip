229c439a938e5c50f5402f8621be8dbfabeee150
requiem <requiem@century-os.com>
2008-11-07 22:52:22 +0000

    * Created skeleton files for Logger and Configuration classes * Trivial typo in SoftChip.cpp comments

---
+++ softchip/source/Logger/Logger.cpp
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Logger.cpp
+ *
+ * Copyright (c) 2008 Requiem (requiem@century-os.com)
+ *
+ * Distributed under the terms of the GNU General Public License (v3)
+ * See http://www.gnu.org/licenses/gpl-3.0.txt for more info.
+ *
+ * Description:
+ * -----------
+ *	Contains implementation of a class to log program output
+ *
+ ******************************************************************************/
+
+#pragma once
+
+//--------------------------------------
+// Includes
+
+//--------------------------------------
+// Logger Class
+
+
+
